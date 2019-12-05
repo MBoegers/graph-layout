4505a0ab845b394473d7afcd931e17dbfbb8b560
Merlin Bögershausen <merlin.boegershausen@rwth-aachen.de>
2019-12-05 18:20:26 +0100

    Starting to work on 1.1-SNAPSHOT

--- release.sh
+++ release.sh
@@ -3,12 +3,12 @@
 #mvn -q versions:set -DnewVersion=$CURRENT_VERSION && \
 #mvn -q clean deploy && \
 #mvn deploy -Dregistry=https://maven.pkg.github.com/MBoegers -Dtoken=074009c3f8cde9634e01f3e354fc9427e1783bc2 && \
-mvn -q versions:commit && \
-git add --all && \
-git commit -m "Created release version $CURRENT_VERSION" && \
-git tag $CURRENT_VERSION && \
-git push --tags && \
-git push --tags github && \
+#mvn -q versions:commit && \
+#git add --all && \
+#git commit -m "Created release version $CURRENT_VERSION" && \
+#git tag $CURRENT_VERSION && \
+#git push --tags && \
+#git push --tags github && \
 git checkout -B dev && \
 read -p "Next version (SNAPSHOT will be added automatically): " NEXT_VERSION && \
 mvn -q versions:set -DnewVersion="$NEXT_VERSION-SNAPSHOT" && \
@@ -16,5 +16,5 @@ mvn -q versions:commit && \
 mvn -q clean verify && \
 git add --all && \
 git commit -m "Starting to work on $NEXT_VERSION-SNAPSHOT" && \
-git push --all && \
-git push --all github
+git push --all #&& \
+#git push --all github
